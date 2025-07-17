847cac8cc39e097350115923d3f4ac05889206e6
AhsRoz <dvirkallush07@gmail.com>
2025-07-17 14:26:00 +0300

    vents

--- addons/autotileplugin/autotileplugin.gd
+++ addons/autotileplugin/autotileplugin.gd
@@ -2,29 +2,53 @@
 extends EditorPlugin
 
 var button: Button
+var spin_box: SpinBox
+var container: HBoxContainer
 var editor_interface: EditorInterface
-@export var source_id: int = 3  # Configurable in the editor
+
+# This is the placeholder source ID we detect in the TileMap
+const PLACEHOLDER_SOURCE_ID := 3
 
 func _enter_tree() -> void:
 	editor_interface = get_editor_interface()
+
+	# Container for SpinBox + Button
+	container = HBoxContainer.new()
+
+	# Create SpinBox for target source ID input
+	spin_box = SpinBox.new()
+	spin_box.min_value = 0
+	spin_box.max_value = 9999
+	spin_box.step = 1
+	spin_box.value = 0
+	spin_box.custom_minimum_size = Vector2(80, 0)
+	spin_box.size_flags_horizontal = Control.SIZE_SHRINK_CENTER
+	container.add_child(spin_box)
+
+	# Create and connect Button
 	button = Button.new()
-	button.text = "Autotile Selected Tiles"
+	button.text = "Autotile Placeholder Tiles"
 	button.pressed.connect(_on_button_pressed)
-	add_control_to_bottom_panel(button, "Autotile")
+	container.add_child(button)
+
+	# Add to bottom panel
+	add_control_to_bottom_panel(container, "Autotile")
 
 func _on_button_pressed() -> void:
-	print("Button pressed")
+	var target_source_id = int(spin_box.value)
+	print("Replacing tiles from ID", PLACEHOLDER_SOURCE_ID, "to", target_source_id)
+
 	var selection = editor_interface.get_selection()
 	var selected_nodes = selection.get_selected_nodes()
-	print("Selected nodes:", selected_nodes)  # Debug
+
 	for node in selected_nodes:
 		if node is TileMap:
-			var tilemap: TileMap = node
-			print("Processing TileMap:", tilemap.name)
-			if tilemap.has_method("update_all_tiles"):
-				tilemap.update_all_tiles(source_id)
+			if node.has_method("update_all_tiles"):
+				node.update_all_tiles(PLACEHOLDER_SOURCE_ID, target_source_id)
 				editor_interface.mark_scene_as_unsaved()
+			else:
+				push_error("TileMap does not have 'update_all_tiles' method!")
 
 func _exit_tree() -> void:
-	if button:
-		button.queue_free()
+	if container:
+		container.queue_free()
