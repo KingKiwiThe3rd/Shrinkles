f1ee88bca3c8f4d7822b6e9067590d1dfa03c104
KingKiwiThe3rd <Akivaeagle12@gmail.com>
2025-07-21 00:55:16 +0300

    moved everything around

--- Scripts/time_manager.gd
+++ Scripts/time_manager.gd
@@ -1,1 +1,14 @@
 extends Node
+
+var game_time := 0.0
+var timer_running := false
+
+func _process(delta):
+	if timer_running:
+		game_time += delta
+
+func reset_timer():
+	game_time = 0.0
+
+func get_time() -> float:
+	return game_time
