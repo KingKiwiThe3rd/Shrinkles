db015ab63b3a30be6ea792e1b34e9675d473b231
KingKiwiThe3rd <Akivaeagle12@gmail.com>
2025-07-20 14:12:24 +0300

    broke the doors

--- Scripts/locked_door.gd
+++ Scripts/locked_door.gd
@@ -1,21 +1,23 @@
 extends StaticBody2D
 
-func _on_KeyCheck_body_entered(body):
-	if body.has_method("has_keycard") and body.has_keycard:
-		open_door()
-
 func open_door():
 	$CollisionShape2D.set_deferred("disabled", true)
-	$Sprite2D.visible = false
+	#$AnimationPlayer.play("opened")
 	if get_tree().get_first_node_in_group("player") as CharacterBody2D:
 		var player = get_tree().get_first_node_in_group("player") as CharacterBody2D
 		if player.keycard:
 			player.keycard.queue_free()
 			player.keycard = null
 			player.has_keycard = false
+	#await $AnimationPlayer.animation_finished
 	call_deferred("queue_free")  # optional: if you want to remove the door itself too
-
-
+#
+#func open_door():
+	#$CollisionShape2D.set_deferred("disabled", true)
+	#var anim_player = $AnimationPlayer
+	#anim_player.play("opened")
+	#await anim_player.animation_finished
+	#queue_free()  # only if you want it gone
 func _on_scanner_body_entered(body: Node2D) -> void:
 	print ("yes")
 	if body is CharacterBody2D:
