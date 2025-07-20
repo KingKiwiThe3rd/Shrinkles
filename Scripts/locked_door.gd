95601a1951ee3139af574a23ca432bcfdd983ed0
KingKiwiThe3rd <Akivaeagle12@gmail.com>
2025-07-20 15:19:09 +0300

    giving up on room animations

--- Scripts/locked_door.gd
+++ Scripts/locked_door.gd
@@ -1,7 +1,7 @@
 extends StaticBody2D
 
 func open_door():
-	$CollisionShape2D.set_deferred("disabled", true)
+	#$CollisionShape2D.set_deferred("disabled", true)
 	#$AnimationPlayer.play("opened")
 	if get_tree().get_first_node_in_group("player") as CharacterBody2D:
 		var player = get_tree().get_first_node_in_group("player") as CharacterBody2D
@@ -11,13 +11,8 @@ func open_door():
 			player.has_keycard = false
 	#await $AnimationPlayer.animation_finished
 	#call_deferred("queue_free")  # optional: if you want to remove the door itself too
-#
-#func open_door():
-	#$CollisionShape2D.set_deferred("disabled", true)
-	#var anim_player = $AnimationPlayer
-	#anim_player.play("opened")
-	#await anim_player.animation_finished
-	#queue_free()  # only if you want it gone
+
+
 func _on_scanner_body_entered(body: Node2D) -> void:
 	print ("yes")
 	if body is CharacterBody2D:
